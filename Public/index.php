1f39722a79ec71202c72c07c898aedf2e067a5bf
wangwenshuai <171997274@qq.com>
2017-03-14 14:03:55 +0800

    20170314

--- Public/index.php
+++ Public/index.php
@@ -4,5 +4,6 @@ define("APP_PATH","../APP/");
 define('RUNTIME_PATH','../App/runtimess/');
 define('APP_DEBUG',true);
 require "../tp/ThinkPHP/ThinkPHP.php";
+// require "../tp/ThinkPHP/ThinkPHP.php";
 
 ?>
